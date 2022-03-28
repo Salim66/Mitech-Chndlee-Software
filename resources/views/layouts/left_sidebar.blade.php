e66221ebe550b2c0c18616a24666c6515d3b67b7
Salim66 <salimhasanriad8@gmail.com>
2022-03-28 18:31:04 +0600

    Users Middleware add

--- resources/views/layouts/left_sidebar.blade.php
+++ resources/views/layouts/left_sidebar.blade.php
@@ -12,23 +12,60 @@
         </div>
         <ul class="sidebar-menu">
             <li><a class="sidebar-header" href="index.html"><i data-feather="home"></i><span>Dashboard</span></a></li>
+
+            @if(Auth::user()->users == 1)
             <li><a class="sidebar-header" href="#"><i data-feather="user-plus"></i><span>Users</span><i class="fa fa-angle-right pull-right"></i></a>
                 <ul class="sidebar-submenu">
                     <li><a href="{{ route('users.list') }}"><i class="fa fa-circle"></i>User List</a></li>
                     <li><a href="{{ route('add.user') }}"><i class="fa fa-circle"></i>Create User</a></li>
                 </ul>
             </li>
+            @endif
+
+            @if(Auth::user()->entery_passport == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="clipboard"></i><span>Entry Passport</span></a></li>
+            @endif
+
+            @if(Auth::user()->test_medical == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="tag"></i><span>Test Medical</span></a></li>
+            @endif
+
+            @if(Auth::user()->final_medical == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="align-left"></i><span>Final Medical</span></a></li>
+            @endif
+
+            @if(Auth::user()->police_clearance == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="bar-chart"></i><span>Police Clearance</span></a></li>
+            @endif
+
+            @if(Auth::user()->mofa == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="chrome"></i><span>Mofa</span></a></li>
+            @endif
+
+            @if(Auth::user()->visa == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="box"></i><span>Visa</span></a></li>
+            @endif
+
+            @if(Auth::user()->traning_certificate == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="shopping-bag"></i><span>Training Certificate</span></a></li>
+            @endif
+
+            @if(Auth::user()->man_power == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="users"></i><span>Man Power</span></a></li>
+            @endif
+
+            @if(Auth::user()->flight == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="navigation"></i><span>Flight</span></a></li>
+            @endif
+
+            @if(Auth::user()->accounts == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="plus"></i><span>Accounts</span></a></li>
+            @endif
+
+            @if(Auth::user()->agent == 1)
             <li><a class="sidebar-header" href="media.html"><i data-feather="user"></i><span>Agent</span></a></li>
+            @endif
+
         </ul>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
 
+
+
 // Admin All Routes
+
+// Admin Logout Route
 Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
 
-// User List Routes
-Route::prefix('users')->group(function () {
-    Route::get('/list', [AdminController::class, 'usersList'])->name('users.list');
-    Route::get('/create', [AdminController::class, 'usersCreate'])->name('add.user');
-    Route::post('/store', [AdminController::class, 'usersStore'])->name('store.user');
-    Route::get('/edit/{id}', [AdminController::class, 'usersEdit'])->name('edit.user');
-    Route::patch('/update/{id}', [AdminController::class, 'usersUpdate'])->name('update.user');
-    Route::get('/delete/{id}', [AdminController::class, 'usersDelete'])->name('delete.user');
-    Route::get('/trash-list', [AdminController::class, 'usersTrashList'])->name('trash.user');
-    Route::get('/revocer/{id}', [AdminController::class, 'usersRecover'])->name('recover.user');
-    Route::get('/permanent-delete/{id}', [AdminController::class, 'usersPermanentDelete'])->name('permanent.delete.user');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+    // User List Routes
+    Route::prefix('users')->group(function () {
+        Route::get('/list', [AdminController::class, 'usersList'])->name('users.list');
+        Route::get('/create', [AdminController::class, 'usersCreate'])->name('add.user');
+        Route::post('/store', [AdminController::class, 'usersStore'])->name('store.user');
+        Route::get('/edit/{id}', [AdminController::class, 'usersEdit'])->name('edit.user');
+        Route::patch('/update/{id}', [AdminController::class, 'usersUpdate'])->name('update.user');
+        Route::get('/delete/{id}', [AdminController::class, 'usersDelete'])->name('delete.user');
+        Route::get('/trash-list', [AdminController::class, 'usersTrashList'])->name('trash.user');
+        Route::get('/revocer/{id}', [AdminController::class, 'usersRecover'])->name('recover.user');
+        Route::get('/permanent-delete/{id}', [AdminController::class, 'usersPermanentDelete'])->name('permanent.delete.user');
+    });
+
 });
